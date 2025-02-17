0798dc86bca0764507fb450392fa8731430cb841
tuq <tuq_5986@163.com>
2025-02-17 17:20:49 +0800

    feat: 路由配置完成

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout.tsx";
-import ManageLayout from "../layouts/ManageLayout";
-import QuestionLayout from "../layouts/QuestionLayout";
+import ManageLayout from "../layouts/ManageLayout.tsx";
+import QuestionLayout from "../layouts/QuestionLayout.tsx";
 import Home from "../pages/Home.tsx";
 import Login from "../pages/Login.tsx";
 import Register from "../pages/Register.tsx";
 import NotFound from "../pages/NotFound.tsx";
-import List from "../pages/manage/List";
-import Trash from "../pages/manage/Trash";
-import Star from "../pages/manage/Star";
-import Edit from "../pages/question/Edit";
-import Stat from "../pages/question/Stat";
+import List from "../pages/manage/List.tsx";
+import Trash from "../pages/manage/Trash.tsx";
+import Star from "../pages/manage/Star.tsx";
+import Edit from "../pages/question/Edit/index.tsx";
+import Stat from "../pages/question/Stat/index.tsx";
 
 const router = createBrowserRouter([
     {
@@ -32,8 +32,40 @@ const router = createBrowserRouter([
                 element: <Register/>,
             },
             {
+                path: '/manage',
+                element: <ManageLayout/>,
+                children: [
+                    {
+                        path: 'list',
+                        element: <List/>
+                    },
+                    {
+                        path: 'star',
+                        element: <Star/>
+                    },
+                    {
+                        path: 'trash',
+                        element: <Trash/>
+                    }
+                ]
+            },
+            {
                 path: '*',   //404路由配置都写在最后
                 element: <NotFound/>
+            }
+        ]
+    },
+    {
+        path: '/question',
+        element: <QuestionLayout/>,
+        children: [
+            {
+                path: 'edit/:id',
+                element: <Edit/>
+            },
+            {
+                path: 'stat/:id',
+                element: <Stat/>
             }
         ]
     }
