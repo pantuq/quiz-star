7eb79804042a155efbee2b66854bd92c934f3ff4
tuq <tuq_5986@163.com>
2025-02-18 21:25:04 +0800

    refactor: 使用antD重构ManageLayout

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,25 +1,51 @@
 import React, { memo, FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import styles from './ManageLayout.module.scss'
+import { Button, Divider, Space } from 'antd'
+import { BarsOutlined, DeleteOutlined, PlusOutlined, StarOutlined } from '@ant-design/icons'
 
 const ManageLayout: FC = memo(function ManageLayout() {
+    const nav = useNavigate()
+    const {pathname} = useLocation()
     return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <p>ManageLayout Left</p>
-                <button>创建问卷</button>
-                <br />
-                <a href="#">我的问卷</a>
-                <br />
-                <a href="#">星标问卷</a>
-                <br />
-                <a href="#">回收站</a>
-            </div>
-            <div className={styles.right}>
-                <Outlet />
-            </div>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <Space direction="vertical">
+            <Button type="primary" size="large" icon={<PlusOutlined />}>
+              新建问卷
+            </Button>
+            <Divider />
+            <Button
+              type={pathname.startsWith('/manage/list') ? 'default' : 'text'}
+              size="large"
+              icon={<BarsOutlined />}
+              onClick={() => nav("/manage/list")}
+            >
+              我的问卷
+            </Button>
+            <Button
+              type={pathname.startsWith('/manage/star') ? 'default' : 'text'}
+              size="large"
+              icon={<StarOutlined />}
+              onClick={() => nav("/manage/star")}
+            >
+              星标问卷
+            </Button>
+            <Button
+              type={pathname.startsWith('/manage/trash') ? 'default' : 'text'}
+              size="large"
+              icon={<DeleteOutlined />}
+              onClick={() => nav("/manage/trash")}
+            >
+              回收站
+            </Button>
+          </Space>
         </div>
-    )
+        <div className={styles.right}>
+          <Outlet />
+        </div>
+      </div>
+    );
 })
 
 
