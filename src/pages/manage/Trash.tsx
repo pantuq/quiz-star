db341e5e3088ceabe993adc58ba4c29e89e01efc
tuq <tuq_5986@163.com>
2025-02-19 20:00:52 +0800

    feat: 开发回收站的恢复和删除，为表格增加了选择的功能

--- src/pages/manage/Trash.tsx
+++ src/pages/manage/Trash.tsx
@@ -1,7 +1,8 @@
-import { Empty, Table, Tag, Typography } from 'antd'
+import { Button, Empty, Modal, Space, Table, Tag, Typography } from 'antd'
 import React, { memo, FC, useState } from 'react'
 import styles from './Common.module.scss'
 import { useTitle } from 'ahooks'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
 
 const rawQuestionList = [
     {
@@ -38,11 +39,25 @@ const rawQuestionList = [
     },
   ]
 
+
 const Trash: FC = memo(function Trash() {
     useTitle('问卷-回收站')
     const [questionList,setQuestionList] = useState(rawQuestionList)
+    // 记录选中的id
+    const [selectIds,setSelectIds] = useState<string[]>([])
 
     const {Title} = Typography
+
+    const { confirm } = Modal
+
+    function del(){
+        confirm({
+            title: '确认彻底删除该问卷?',
+            icon: <ExclamationCircleOutlined/>,
+            content: '删除以后不可以找回',
+            onOk: () => alert('删除' + JSON.stringify(selectIds))
+        })
+    }
 
     const TableCol = [
       {
@@ -69,6 +84,28 @@ const Trash: FC = memo(function Trash() {
         dataIndex: "createdAt",
       },
     ];
+
+    const TableElem = (<>
+        <div style={{marginBottom: '16px'}}>
+            <Space>
+                <Button type='primary' disabled={!selectIds.length}>恢复</Button>
+                <Button danger disabled={!selectIds.length} onClick={del}>彻底删除</Button>
+            </Space>
+        </div>
+
+        <Table
+        dataSource={questionList}
+        columns={TableCol}
+        pagination={false}
+        rowKey={(q => q._id)}
+        rowSelection={{
+        type: 'checkbox',
+        onChange: (selectedRowKeys) => {
+            setSelectIds(selectedRowKeys as string[])
+        }
+        }}
+        />
+    </>)
     return (
       <>
         <div className={styles.header}>
@@ -79,7 +116,7 @@ const Trash: FC = memo(function Trash() {
         </div>
         <div className={styles.content}>
           {questionList.length === 0 && <Empty description="暂无数据" />}
-          {questionList.length > 0 && <Table dataSource={questionList} columns={TableCol} pagination={false}/>}
+          {questionList.length > 0 && TableElem }
         </div>
       </>
     );
