ba8439c3aae26f1c5217f3f644fdb6e7d02984e5
tuq <tuq_5986@163.com>
2025-02-19 12:17:46 +0800

    feat: 复制与删除按钮的交互

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,7 @@
 import React, { memo, FC } from 'react'
 import styles from './QuestionCard.module.scss'
-import { Button, Divider, Space, Tag } from 'antd'
-import { CopyOutlined, DeleteOutlined, EditOutlined, LineChartOutlined, StarOutlined } from '@ant-design/icons'
+import { Button, Divider, message, Modal, Popconfirm, Space, Tag } from 'antd'
+import { CopyOutlined, DeleteOutlined, EditOutlined, ExclamationCircleOutlined, LineChartOutlined, StarOutlined } from '@ant-design/icons'
 import { Link, useNavigate } from 'react-router-dom'
 
 type PropsType = {
@@ -15,7 +15,23 @@ type PropsType = {
 
 const QuestionCard: FC<PropsType> = memo(function QuestionCard(props: PropsType) {
   const {_id, title, createdAt, answerCount, isPublished, isStar} = props
+
   const nav = useNavigate()
+
+  const {confirm} = Modal
+
+  function duplicate() {
+    message.success('复制成功')
+  }
+
+  function del(){
+    confirm({
+      title: '确定要删除吗？',
+      content: '删除后不可恢复',
+      icon: <ExclamationCircleOutlined/>,
+      onOk: () => message.success('删除成功')
+    })
+  }
     return (
       <div className={styles.container}>
         <div className={styles.title}>
@@ -34,18 +50,18 @@ const QuestionCard: FC<PropsType> = memo(function QuestionCard(props: PropsType)
           <div className={styles.right}>
             <Space>
               {isPublished ? (
-                <Tag color='processing'>已发布</Tag>
+                <Tag color="processing">已发布</Tag>
               ) : (
                 <Tag>未发布</Tag>
               )}
-              
+
               <span>答卷: {answerCount}</span>
-              
+
               <span>创建时间: {createdAt}</span>
             </Space>
           </div>
         </div>
-        <Divider style={{margin: '12px 0'}}/>
+        <Divider style={{ margin: "12px 0" }} />
         <div className={styles["button-container"]}>
           <div className={styles.left}>
             <Space>
@@ -73,10 +89,22 @@ const QuestionCard: FC<PropsType> = memo(function QuestionCard(props: PropsType)
               <Button type="text" icon={<StarOutlined />} size="small">
                 {isStar ? "取消标星" : "标星"}
               </Button>
-              <Button type="text" icon={<CopyOutlined />} size="small">
-                复制
-              </Button>
-              <Button type="text" icon={<DeleteOutlined />} size="small">
+              <Popconfirm
+                title="确定复制该问卷？"
+                okText="确定"
+                cancelText="取消"
+                onConfirm={duplicate}
+              >
+                <Button type="text" icon={<CopyOutlined />} size="small">
+                  复制
+                </Button>
+              </Popconfirm>
+              <Button
+                type="text"
+                icon={<DeleteOutlined />}
+                size="small"
+                onClick={del}
+              >
                 删除
               </Button>
             </Space>
