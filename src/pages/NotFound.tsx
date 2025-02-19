a5384bcede5c70274c154ca11a0494cb5a2f40a5
tuq <tuq_5986@163.com>
2025-02-19 11:16:14 +0800

    feat: 使用antD制作漂亮的404页面

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,18 @@
+import { Button, Result } from 'antd'
 import React, { memo, FC } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { HOME_PATHNAME } from '../router/index.tsx';
 
 const NotFound: FC = memo(function NotFound() {
+    const nav = useNavigate()
     return (
-        <>
-            NotFound
-        </>
-    )
+      <Result
+        status="404"
+        title="404"
+        subTitle="抱歉，您访问的页面不存在"
+        extra={<Button type="primary" onClick={() => nav(HOME_PATHNAME)}>Back Home</Button>}
+      />
+    );
 })
 
 
