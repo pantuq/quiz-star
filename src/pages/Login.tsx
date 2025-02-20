48ac4b24e82cc0a7a7e3a751dc27a4418820155a
tuq <tuq_5986@163.com>
2025-02-20 19:53:46 +0800

    feat: 开发登录页

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,96 @@
-import React, { memo, FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { memo, FC, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Login.module.scss'
+import { Button, Checkbox, Form, Input, Space, Typography } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { REGISTER_PATHNAME } from '../router/index.tsx'
 
+const USERNAME_KEY = "USERNAME"
+const PASSWORD_KEY = "PASSWORD"
+
+function rememberUser(username: string, password: string){
+    localStorage.setItem(USERNAME_KEY, username)
+    localStorage.setItem(PASSWORD_KEY, password)
+}
+
+function deleteUserFromStorage(){
+    localStorage.removeItem(USERNAME_KEY)
+    localStorage.removeItem(PASSWORD_KEY)
+}
+
+function getUserInfoFromStorage(){
+    return {
+        username: localStorage.getItem(USERNAME_KEY),
+        password: localStorage.getItem(PASSWORD_KEY)
+    }
+}
 const Login: FC = memo(function Login() {
-    const nav = useNavigate()
+
+    const [form] = Form.useForm() //第三方 hook
+
+    useEffect(() => {
+        const { username, password } = getUserInfoFromStorage()
+        form.setFieldsValue({
+            username,
+            password
+        })
+    },[])
+    // 没有写依赖项，只在函数组件开始渲染的时候执行一次
+
+    const onFinish = (value: object) => {
+        console.log(value);
+        const { username, password, remember } = value;
+        if(remember){
+            rememberUser(username, password)
+        }else {
+            deleteUserFromStorage()
+        }
+    }
+
+    const { Title } = Typography
     return (
-        <>
-            Login
-            <div>
-                <button onClick={() => nav(-1)}>返回</button>
-            </div>
-        </>
-    )
+      <div className={styles.container}>
+        <div>
+          <Space>
+            <Title level={2}>
+              <UserAddOutlined />
+            </Title>
+            <Title level={2}>用户登录</Title>
+          </Space>
+        </div>
+        <div>
+          <Form
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            form={form}
+          >
+            <Form.Item label="用户名" name="username">
+              <Input />
+            </Form.Item>
+            <Form.Item label="密码" name="password">
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 6, span: 16 }}
+            >
+              <Checkbox>记住我</Checkbox>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+              <Space>
+                <Button type="primary" htmlType="submit">
+                  登录
+                </Button>
+                <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+              </Space>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    );
 })
 
 
