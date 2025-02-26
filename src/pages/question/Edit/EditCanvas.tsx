7bcec16391c3f68903a438b0827b4281692026e3
tuq <tuq_5986@163.com>
2025-02-26 20:02:26 +0800

    feat: 在画布上动态显示组件列表

--- src/pages/question/Edit/EditCanvas.tsx
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -4,10 +4,23 @@ import QuestionTitle from '../../../components/QuestionComponents/QuestionTitle/
 import QuestionInput from '../../../components/QuestionComponents/QuestionInput/Component.tsx'
 import { Spin } from 'antd'
 import useGetCompoentsInfo from '../../../hooks/useGetComponentsInfo.ts'
+import { ComponentInfoType } from '../../../store/componentsReducer/index.ts'
+import { getComponentConfByType } from '../../../components/QuestionComponents/index.ts'
 
 type PropsType = {
     loading: boolean
 }
+
+function genComponent(componentInfo: ComponentInfoType){
+    const { type, props } = componentInfo
+
+    const componentConf = getComponentConfByType(type)
+    if(!componentConf) return null
+    
+    const { Component } = componentConf
+    return <Component {...props}/>
+}
+
 const EditCanvas: FC<PropsType> = memo(function EditCanvas(props: PropsType) {
     const { componentList } = useGetCompoentsInfo()
     console.log('componentList',componentList);
@@ -19,7 +32,17 @@ const EditCanvas: FC<PropsType> = memo(function EditCanvas(props: PropsType) {
     }
     return (
         <div className={styles.canvas}>
-            <div className={styles['component-wrapper']}>
+            {componentList.map(c => {
+                const { fe_id } = c 
+                return (
+                    <div key={fe_id} className={styles['component-wrapper']}>
+                        <div className={styles.component}>
+                            {genComponent(c)}
+                        </div>
+                    </div>
+                )
+            })}
+            {/* <div className={styles['component-wrapper']}>
                 <div className={styles.component}>
                     <QuestionTitle/>
                 </div>
@@ -28,7 +51,7 @@ const EditCanvas: FC<PropsType> = memo(function EditCanvas(props: PropsType) {
                 <div className={styles.component}>
                     <QuestionInput/>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 })
