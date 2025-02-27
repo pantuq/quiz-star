62c84b97939722082be02beca463cbb228c0365c
tuq <tuq_5986@163.com>
2025-02-27 19:28:13 +0800

    feat: 把组件分组显示在组件库中

--- src/pages/question/Edit/ComponentLib.tsx
+++ src/pages/question/Edit/ComponentLib.tsx
@@ -1,16 +1,29 @@
 import React, { memo, FC } from 'react'
-import { componentConfGroup } from '../../../components/QuestionComponents/index.ts'
+import { componentConfGroup, ComponentConfType } from '../../../components/QuestionComponents/index.ts'
 import { Typography } from 'antd'
+import styles from './ComponentLib.module.scss'
+
+function genComponent(c: ComponentConfType){
+    const { title,type, Component } = c
+    return <div className={styles.wrapper}>
+        <div className={styles.component}>
+        <Component/>
+        </div>
+    </div>
+}
 
 const ComponentLib: FC = memo(function ComponentLib() {
     const { Title } = Typography
     return (
         <>
             {componentConfGroup.map((group,index) => {
-                const { groupId, groupName} = group
+                const { groupId, groupName,components} = group
                 return (
                     <div key={groupId}>
                         <Title level={3} style={{ fontSize: '20px', marginTop: index > 0 ? '20px' : '0'}}>{groupName}</Title>
+                        <div>
+                            {components.map(c => genComponent(c))}
+                        </div>
                     </div>
                 )
             })}
