d839ded0e04025e882e95432ae5d7cdf50677f62
tuq <tuq_5986@163.com>
2025-03-03 17:00:52 +0800

    fix: 为Paragraph增加换行-考虑安全性

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -7,10 +7,16 @@ const { Paragraph } = Typography
 const Component: FC<QuestionParagraphPropsType> = memo(function Component(props: QuestionParagraphPropsType) {
     const { text = '', isCenter = false } = {...QuestionParagraphDefaultProps, ...props}
 
-    const t = text.replaceAll('\n','<br>')
+    const textList = text.split('\n')
     return (
         <Paragraph style={{ textAlign: isCenter ? 'center': 'start', marginBottom: 0}}>
-            <span dangerouslySetInnerHTML={{__html: t}}></span>
+            {
+                textList.map((t,index) => {
+                    return (
+                        <span key={index}>{index > 0 && <br/>} {t}</span>
+                    )
+                })
+            }
         </Paragraph>
     )
 })
