f43baddfcd6cbd46ff11e682c46c6c992ad96cc7
tuq <tuq_5986@163.com>
2025-03-05 20:51:49 +0800

    feat: 保存问卷信息-判断loading防止连续触发

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,12 +1,15 @@
 import React, { memo, FC, useState, ChangeEvent } from 'react'
 import styles from './EditHeader.module.scss'
 import { Button, Input, Space, Typography } from 'antd'
-import { EditOutlined, LeftOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom'
+import { EditOutlined, LeftOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useNavigate, useParams } from 'react-router-dom'
 import EditToolbar from './EditToolbar.tsx'
 import useGetPageInfo from '../../../hooks/useGetPageInfo.ts'
 import { useDispatch } from 'react-redux'
 import { changePageTitle } from '../../../store/pageInfoReducer.tsx'
+import useGetCompoentsInfo from '../../../hooks/useGetComponentsInfo.ts'
+import { useKeyPress, useRequest } from 'ahooks'
+import { updateQuestionService } from '../../../services/question.ts'
 const { Title } = Typography
 
 // 显示和修改标题
@@ -41,6 +44,37 @@ const TitleElem: FC = memo(function TitleElem(){
   )
 })
 
+// 保存按钮
+const SaveButton: FC = memo(function SaveButton(){
+  const pageInfo = useGetPageInfo()
+  const { componentList } = useGetCompoentsInfo()
+  const { id } = useParams()
+
+  const { loading, run: save } = useRequest(async () => {
+    if(id){
+      await updateQuestionService(id, { ...pageInfo, componentList })
+    }
+  },{
+    manual: true
+  })
+
+  // 快捷键
+  useKeyPress(['ctrl.s','meta.s'],(event: KeyboardEvent) => {
+    event.preventDefault()
+    if(!loading) save()
+  })
+
+  return (
+    <Button
+      onClick={save}
+      disabled={loading}
+      icon={loading ? <LoadingOutlined /> : null}
+    >
+      保存
+    </Button>
+  );
+})
+
 // 编辑器头部组件
 const EditHeader: FC = memo(function EditHeader() {
   const nav = useNavigate()
@@ -64,7 +98,7 @@ const EditHeader: FC = memo(function EditHeader() {
           </div>
           <div className={styles.right}>
             <Space>
-                <Button>保存</Button>
+                <SaveButton/>
                 <Button type='primary'>发布</Button>
             </Space>
           </div>
