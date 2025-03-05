5fafd8e0462acfba22b379fe597ae05b3b1648bf
tuq <tuq_5986@163.com>
2025-03-05 18:32:14 +0800

    feat: 设置页面信息-修改表单同步到redux中

--- src/pages/question/Edit/PageSetting.tsx
+++ src/pages/question/Edit/PageSetting.tsx
@@ -1,11 +1,48 @@
-import React, { memo, FC } from 'react'
+import React, { memo, FC, useEffect } from 'react'
+import useGetPageInfo from '../../../hooks/useGetPageInfo.ts'
+import { Form, Input } from 'antd'
+import { useDispatch } from 'react-redux'
+import { resetPageInfo } from '../../../store/pageInfoReducer.tsx'
+
+const { TextArea } = Input
 
 const PageSetting: FC = memo(function PageSetting() {
+    const pageInfo = useGetPageInfo()
+    const [form] = Form.useForm()
+    const dispatch = useDispatch()
+
+    // 实时更新表单
+    useEffect(() => {
+        form.setFieldsValue(pageInfo)
+    },[pageInfo])
+
+    function handleValuesChange(){
+        dispatch(resetPageInfo(form.getFieldsValue()))
+    }
     return (
-        <>
-            PageSetting
-        </>
-    )
+      <Form
+        layout="vertical"
+        initialValues={pageInfo}
+        onValuesChange={handleValuesChange}
+        form={form}
+      >
+        <Form.Item label='问卷标题' name='title' rules={[{ required: true, message: '问卷标题不能为空'}]}>
+            <Input placeholder='请输入标题'/>
+        </Form.Item>
+
+        <Form.Item label='问卷描述' name='desc'>
+            <TextArea/>
+        </Form.Item>
+
+        <Form.Item label='样式代码' name='css'>
+            <TextArea/>
+        </Form.Item>
+
+        <Form.Item label='脚本代码' name='js'>
+            <TextArea/>
+        </Form.Item>
+      </Form>
+    );
 })
 
 
