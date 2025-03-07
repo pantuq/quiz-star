65600c97501aa5272e56039ef47dce37526431ce
tuq <tuq_5986@163.com>
2025-03-07 19:15:33 +0800

    feat: 修改端口号

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     devServer: {
+        port: 8000, // B端 端口号
         proxy: {
             '/api': {  // 添加前导斜杠
-                target: 'http://127.0.0.1:3001',
+                target: 'http://127.0.0.1:3001',    //mock
                 changeOrigin: true
             }
         }
