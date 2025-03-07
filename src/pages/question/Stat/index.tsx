be9d6959934ea523bc5033bc4cd768f2ab2b133a
tuq <tuq_5986@163.com>
2025-03-07 20:13:19 +0800

    feat: 页面UI布局-增加loading效果

--- src/pages/question/Stat/index.tsx
+++ src/pages/question/Stat/index.tsx
@@ -4,6 +4,7 @@ import { Button, Result, Spin } from 'antd'
 import useGetPageInfo from '../../../hooks/useGetPageInfo.ts'
 import { useNavigate } from 'react-router-dom'
 import { useTitle } from 'ahooks'
+import styles from './index.module.scss'
 
 const Stat: FC = memo(function Stat() {
     const nav = useNavigate()
@@ -13,28 +14,41 @@ const Stat: FC = memo(function Stat() {
     // 修改标题
     useTitle(`问卷统计 - ${title}`)
 
-    if(loading){
-        return (
-            <div style={{ textAlign: 'center', marginTop: '60xp'}}>
-                <Spin/>
+    const LoadingElem = (<div style={{ textAlign: 'center', marginTop: '60xp'}}>
+                            <Spin/>
+                        </div>)
+
+    function genContentElem(){
+        if(typeof isPublished === 'boolean' && !isPublished){
+            // 因为没有设默认值，isPublished为undefined 所以要在这里判断一下
+            // 如果isPublshed的类型是布尔值的话，就执行
+            return <div style={{ flex: '1' }}>
+                <Result
+                status="warning"
+                title="该页面尚未发布"
+                subTitle="抱歉，您访问的页面不存在"
+                extra={<Button type="primary" onClick={() => nav(-1)}>返回上一页</Button>}
+                />
             </div>
+        }
+
+        return (
+            <>
+            <div className={styles.left}>左侧</div>
+            <div className={styles.main}>中间</div>
+            <div className={styles.right}>右侧</div>
+            </>
         )
     }
-
-    if(!isPublished){
-        return <Result
-        status="warning"
-        title="该页面尚未发布"
-        subTitle="抱歉，您访问的页面不存在"
-        extra={<Button type="primary" onClick={() => nav(-1)}>返回上一页</Button>}
-      />
-    }
     return (
-        <div>
-            <p>Stat page</p>
-            { loading ? <p>loading</p> : <p></p>}
+      <div className={styles.container}>
+        <div>Header</div>
+        <div className={styles["content-wrapper"]}>
+          {loading && LoadingElem}
+          {!loading && <div className={styles.content}>{genContentElem()}</div>}
         </div>
-    )
+      </div>
+    );
 })
 
 
--- src/store/pageInfoReducer.tsx
+++ src/store/pageInfoReducer.tsx
@@ -14,7 +14,6 @@ const INIT_STATE: PageInfoType = {
     desc: '',
     js: '',
     css: '',
-    isPublished: false
 }
 
 const pageInfoSlice = createSlice({
