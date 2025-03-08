21ff948fd5e9c758a1223ec574e1b46f9c46ba49
tuq <tuq_5986@163.com>
2025-03-08 22:01:14 +0800

    feat: 显示统计表格

--- src/pages/question/Stat/PageStat.tsx
+++ src/pages/question/Stat/PageStat.tsx
@@ -2,7 +2,8 @@ import { useRequest } from 'ahooks'
 import React, { memo, FC, useState } from 'react'
 import { getQuestionStatListService } from '../../../services/stat.ts'
 import { useParams } from 'react-router-dom'
-import { Spin, Typography } from 'antd'
+import { Spin, Table, Typography } from 'antd'
+import useGetCompoentsInfo from '../../../hooks/useGetComponentsInfo.ts'
 
 const { Title } = Typography
 
@@ -13,6 +14,7 @@ type PropsType = {
   }
 
 const PageStat: FC<PropsType> = memo(function PageStat(props: PropsType) {
+    const { selectedComponentId, setSelectedComponentId, setSelectedComponentType } = props
     const { id = '' } = useParams()
     const [total, setTotal] = useState(0)
     const [list, setList] = useState([])
@@ -23,13 +25,36 @@ const PageStat: FC<PropsType> = memo(function PageStat(props: PropsType) {
         onSuccess(res){
             const { total, list = [] } = res
             setTotal(total)
-            setList(list)
+            setList(list)            
         }
     })
+
+    const { componentList } = useGetCompoentsInfo()
+    const colums = componentList.map(c => {
+        const { fe_id, title, props = {}, type } = c
+        const colTitle = props!.title || title
+
+        return {
+            title: (
+                <div style={{ cursor: 'pointer'}} onClick={() => {
+                    setSelectedComponentId(fe_id)
+                    setSelectedComponentType(type)
+                }}>
+                    <span style={{ color: fe_id === selectedComponentId ? '#1890ff' : '' }}>{colTitle}</span>
+                </div>
+            ),
+            dataIndex: fe_id
+        }
+    })    
+
+    const dataSource = list.map((item: any) => ({...item, key: item._id}))
+    const TableElem = <Table columns={colums} dataSource={dataSource} pagination={false}></Table>
+    
     return (
         <div>
             <Title>答卷数量： {!loading && total}</Title>
             {loading && (<div style={{ textAlign: 'center' }}><Spin/></div>)}
+            {!loading && TableElem}
         </div>
     )
 })
